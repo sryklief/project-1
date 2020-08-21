54575d93a7985508173d75861ac4407d7803b5bd
sryklief <61458785+sryklief@users.noreply.github.com>
2020-08-21 14:57:04 +0200

    Update Prod3.js

--- pages/Prod3.js
+++ pages/Prod3.js
@@ -1,52 +1,82 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import { Row } from 'react-bootstrap';
-import data from '../../backend/data';
+import React, { Component } from 'react';
+import bath from './assests/bath.jpg';
+import bowls5 from './assests/bowls5.jpg';
+import Drinker from './assests/Drinker.jpg'
+import feeder from './assests/feeder.jpg';
+import nest_pads from './assests/nest_pads.jpg';
+
 
 class Prod3 extends Component {
     render () {
         return (
-           <div>
-                <div className="container">
-                    <ul className="nav nav-tabs nav-justified">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/prod1" role="tab" data-toggle="tab">Prod1</Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/prod2" role="tab" data-toggle="tab">Prod2</Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className="nav-link" to="#prod3" role="tab" data-toggle="tab">Prod3</Link>
-                        </li>
-                    </ul>
-                </div>
-                    <Row>
-                        {data.products3.map(product =>
-                        <div id="#prod" className="tab-pane in active text-center mt-5" role="tabpanel1">
-                            <div class="proj-card" style={{width: "300px", paddingLeft: '5em'}}>
-                                <img src={product.image} class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                <h5 class="card-title">{product.name}</h5>
-                                    <p class="card-text">High quality loft essentials.</p>
-                                </div>
-                                
+           <div className="prod-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div className="card">
+                            <img class="card-img-top" src={bath} alt="Card image cap"/>
                             <div class="card-body">
-                            <a class="card-link">{product.price}</a>
-                                <a href="#" class="card-link">Add to cart</a>
+                                <h5 class="card-title">Bath</h5>
+                                <p class="card-text">15 bird bath.</p>
                             </div>
+                            <div class="card-body">
+                                <p>R40,00</p>
+                                <a href="/Bath" class="card-link">View Product</a>
                             </div>
                         </div>
-                            )}
-                    </Row>
-                    <div className="footer" style={{backgroundColor: 'grey'}}>
-                        <p className='footer-content' style={{paddingTop: '2rem', paddingBottom: '7rem'}}>
-                            Flokkies Flock<br/>
-                            Email - flokkiesflock@gmail.com<br/>
-                            Contact - 123-456-7890
-                        </p>
                     </div>
+                    <div class="col-sm-3">
+                    <div className="card">
+                        <img class="card-img-top" src={bowls5} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Nest Bowls</h5>
+                                <p class="card-text">Clay nest bowls.</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R20,00</p>
+                                <a href="/Bowl" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                    <div className="card">
+                        <img class="card-img-top" src={nest_pads} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Nest Pads</h5>
+                                <p class="card-text">Nest pads manufactured with natural materials</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R10,00</p>
+                                <a href="/Nest" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                    <div className="card">
+                        <img class="card-img-top" src={Drinker} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Drinker</h5>
+                                <p class="card-text">5 Liter drinker.</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R100,00</p>
+                                <a href="/Drink" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                    <div className="card"  style={{marginBottom: "1rem"}}>
+                        <img class="card-img-top" src={feeder} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Feeding Tray</h5>
+                                <p class="card-text">! meter feeding tray.</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R30,00</p>
+                                <a href="/Feeder" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
