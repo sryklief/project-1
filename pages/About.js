22f09144cb1ae8a6be08117766ae1da37004b6c8
sryklief <61458785+sryklief@users.noreply.github.com>
2020-08-21 14:58:38 +0200

    Update About.js

--- pages/About.js
+++ pages/About.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import headimg2 from '../assests/headimg2.jpg';
+import headimg2 from './assests/headimg2.jpg';
 
 class About extends Component {
     render () {
         return (
         <div>
             <div container>
-                <img src={headimg2} alt="pic" className="head-pic" style={{width: 'fixed', height: '80%'}}/>
+                <img src={headimg2} alt="pic" className="head-pic" style={{width: '100%', height: '80%'}}/>
                 <div className="centered">
-                    <h1 className="heading" style={{color: 'white'}}>About Us</h1>
+                    <h1 class="center" style={{color: 'black'}}>About Us</h1>
                     <p className="about-contents" style={{color: 'white'}}>
                     Here At Flokkies Flock Our Goal Is Very Simple; To Provide The Widest Range Of Bird Food And 
                     Supplies At The Lowest Possible Prices. We Strive To Provide Low Price, Expert Customer Service 
@@ -17,15 +17,7 @@ class About extends Component {
                 </p>
                 </div>
             </div>
-            <div className="footer" style={{backgroundColor: 'grey'}}>
-            <p className='footer-content' style={{paddingTop: '2rem', paddingBottom: '2rem'}}>
-                Flokkies Flock<br/>
-                Email - flokkiesflock@gmail.com<br/>
-                Contact - 123-456-7890
-            </p>
-            </div>
         </div>
-        
         )
     }
 }
