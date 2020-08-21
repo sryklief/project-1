a708252239066af13cdc1cd15d6ffafe29a916a2
sryklief <61458785+sryklief@users.noreply.github.com>
2020-08-21 14:55:27 +0200

    Update Prod1.js

--- pages/Prod1.js
+++ pages/Prod1.js
@@ -1,53 +1,81 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Row } from 'react-bootstrap';
-import data from '../../backend/data';
+import Breeders from './assests/Breeders.jpg' ;
+import racemix from './assests/racemix.jpg';
+import SuperCrack from './assests/SuperCrack.jpg'
+import mielies from './assests/mielies.jpg';
+import sunflower from './assests/sunflower.jpg';
 
 class Prod1 extends Component {
     render () {
         return (
-           <div>
-                <div className="container">
-                    <ul className="nav nav-tabs nav-justified">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="#prod1" role="tab" data-toggle="tab">Prod1</Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/prod2" role="tab" data-toggle="tab">Prod2</Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/prod3" role="tab" data-toggle="tab">Prod3</Link>
-                        </li>
-                    </ul>
-                </div>
-                <div className="tab-content mb-5" >
-                    <Row>
-                        {data.products.map(product =>
-                            <div id="#prod" className="tab-pane in active text-center mt-5" role="tabpanel1">
-                            <div class="proj-card" style={{width: "300px", paddingLeft: '5em'}}>
-                                <img src={product.image} class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                <h5 class="card-title">{product.name}</h5>
-                                    <p class="card-text">High quality feed.</p>
-                                </div>
-                                
+           <div className="prod-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div className="card">
+                            <img class="card-img-top" src={Breeders} alt="Card image cap"/>
                             <div class="card-body">
-                            <a class="card-link">{product.price}</a>
-                                <a href="#" class="card-link">Add to cart</a>
+                                <h5 class="card-title">Breeders 25kg</h5>
+                                <p class="card-text">Quality seed for parents raising chicks.</p>
                             </div>
+                            <div class="card-body">
+                                <p>R160,00</p>
+                                <Link to="/breeder" class="card-link">View Product</Link>
                             </div>
                         </div>
-                            )}
-                    </Row>
-                </div>
-                <div className="footer" style={{backgroundColor: 'grey'}}>
-                    <p className='footer-content' style={{paddingTop: '2rem', paddingBottom: '4rem'}}>
-                        Flokkies Flock<br/>
-                        Email - flokkiesflock@gmail.com<br/>
-                        Contact - 123-456-7890
-                    </p>
+                    </div>
+                    <div class="col-sm-3">
+                    <div className="card">
+                        <img class="card-img-top" src={racemix} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Race Mix 25kg</h5>
+                                <p class="card-text">High energy seed.</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R120,00</p>
+                                <a href="/race" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                    <div className="card">
+                        <img class="card-img-top" src={SuperCrack} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Recovery Mix 25kg</h5>
+                                <p class="card-text">High protein seed for after races.</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R145,00</p>
+                                <a href="Recover" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                    <div className="card">
+                        <img class="card-img-top" src={mielies} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Corn 25kg</h5>
+                                <p class="card-text">Quality corn seed.</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R80,00</p>
+                                <a href="/Corn" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                    <div className="card"  style={{marginBottom: "1rem"}}>
+                        <img class="card-img-top" src={sunflower} alt="Card image cap"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Sunflower</h5>
+                                <p class="card-text">Quality sunflower seed.</p>
+                            </div>
+                            <div class="card-body">
+                                <p>R90,00</p>
+                                <a href="/Sun" class="card-link">View Product</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
