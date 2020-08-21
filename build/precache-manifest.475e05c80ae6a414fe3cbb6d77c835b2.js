34859335b8179966c59ca13952f89f4892927aa3
sryklief <61458785+sryklief@users.noreply.github.com>
2020-08-21 15:15:42 +0200

    Add files via upload

---
+++ build/precache-manifest.475e05c80ae6a414fe3cbb6d77c835b2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe222559fd355cbeff7a561215f82e6",
+    "url": "/Final-Web-Project.git/index.html"
+  },
+  {
+    "revision": "af932d140df9b24efe50",
+    "url": "/Final-Web-Project.git/static/css/2.be82fd29.chunk.css"
+  },
+  {
+    "revision": "c2bf922c9d611abeb0a3",
+    "url": "/Final-Web-Project.git/static/css/main.a2ac6a23.chunk.css"
+  },
+  {
+    "revision": "af932d140df9b24efe50",
+    "url": "/Final-Web-Project.git/static/js/2.3feb92fb.chunk.js"
+  },
+  {
+    "revision": "aba786479f25e17854d61dc6e4e87472",
+    "url": "/Final-Web-Project.git/static/js/2.3feb92fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2bf922c9d611abeb0a3",
+    "url": "/Final-Web-Project.git/static/js/main.4972edd5.chunk.js"
+  },
+  {
+    "revision": "c5060b4180162b9dfa9b",
+    "url": "/Final-Web-Project.git/static/js/runtime-main.547e3b32.js"
+  },
+  {
+    "revision": "439fa423a059010201cc6b2c3d50e7ff",
+    "url": "/Final-Web-Project.git/static/media/Breeders.439fa423.jpg"
+  },
+  {
+    "revision": "35514b162aa9ce961c3f37e0ab593fc9",
+    "url": "/Final-Web-Project.git/static/media/Drinker.35514b16.jpg"
+  },
+  {
+    "revision": "81dc5a8c10fe439a3c38d0cce9137d3a",
+    "url": "/Final-Web-Project.git/static/media/PMV.81dc5a8c.jpg"
+  },
+  {
+    "revision": "41b2d1f5f6f0ee3ddaae40ffee4a2c5a",
+    "url": "/Final-Web-Project.git/static/media/SuperCrack.41b2d1f5.jpg"
+  },
+  {
+    "revision": "344da0e2b565a2c89695368271837717",
+    "url": "/Final-Web-Project.git/static/media/canker.344da0e2.png"
+  },
+  {
+    "revision": "d0065fe36d363018e66f0cb349c8ffea",
+    "url": "/Final-Web-Project.git/static/media/headimg2.d0065fe3.jpg"
+  },
+  {
+    "revision": "dd33e0d874e6f0aaf230199d08fdf75b",
+    "url": "/Final-Web-Project.git/static/media/immune.dd33e0d8.jpg"
+  },
+  {
+    "revision": "221c1295d4a25770c9b4ad1f314a84c0",
+    "url": "/Final-Web-Project.git/static/media/malaria.221c1295.jpg"
+  },
+  {
+    "revision": "f74f2f82ecc8f3f97a4475a1565f6f4a",
+    "url": "/Final-Web-Project.git/static/media/mielies.f74f2f82.jpg"
+  },
+  {
+    "revision": "12c8ec999d27f47066916859e11e5c3d",
+    "url": "/Final-Web-Project.git/static/media/racemix.12c8ec99.jpg"
+  },
+  {
+    "revision": "7b7badd18692de5ebd4f2b5c64c37f76",
+    "url": "/Final-Web-Project.git/static/media/simplewallpaper.7b7badd1.png"
+  },
+  {
+    "revision": "deaf13dee10dda9bb7366a1535eb10a8",
+    "url": "/Final-Web-Project.git/static/media/sunflower.deaf13de.jpg"
+  },
+  {
+    "revision": "33a511e4c946fdca33f33e68943829db",
+    "url": "/Final-Web-Project.git/static/media/worms.33a511e4.jpg"
+  }
+]);
